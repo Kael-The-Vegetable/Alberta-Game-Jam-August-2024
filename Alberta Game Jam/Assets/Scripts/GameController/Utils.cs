8313e2b06c2867b67b8846f0238d5759c63cad21
Jonathan Dodds <144161210+JDoddsNAIT@users.noreply.github.com>
2024-08-10 13:47:43 -0600

    Revert "Created a global random instance"

--- Alberta Game Jam/Assets/Scripts/GameController/Utils.cs
+++ Alberta Game Jam/Assets/Scripts/GameController/Utils.cs
@@ -3,5 +3,9 @@
 public static class Utils
 {
     // Selects a random element of an array
-    public static T SelectRandomElement<T>(this T[] values) => values[Singleton.Global.Random.Next(values.Length)];
+    public static T SelectRandomElement<T>(this T[] values)
+    {
+        Random random = new();
+        return values[random.Next(values.Length)];
+    }
 }
