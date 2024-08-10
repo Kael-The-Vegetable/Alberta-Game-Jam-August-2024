4917889852cc075c69c1ccdafb60a7dd9500f606
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-08-10 14:20:41 -0600

    created text printer object

---
+++ Alberta Game Jam/Assets/Scripts/TextPrinter/TextPrinter.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextPrinter : MonoBehaviour
+{
+    public float charactersPerSecond;
+
+    public TextMeshProUGUI label;
+    [TextArea]
+    string text;
+
+    private void Awake()
+    {
+        // cache the labels text.
+        text = label.text;
+        label.text = string.Empty;
+
+        // TODO: set charactersPerSecond the variable set in the settings object
+    }
+
+    public void BeginPrint()
+    {
+        StartCoroutine(ShowLetters(1 / charactersPerSecond));
+    }
+
+    private IEnumerator ShowLetters(float delay)
+    {
+        for (int i = 0; i < text.Length; i++)
+        {
+            label.text += text[i];
+            yield return new WaitForSeconds(delay);
+        }
+        yield return new WaitForSeconds(10 * delay);
+    }
+}
