74bdd8bb2f3d3742cb23a9b2f2d1727e25cf8e6d
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-08-10 20:39:27 -0600

    a

--- Alberta Game Jam/Assets/Features/Game-Over/EndAnimation.cs
+++ Alberta Game Jam/Assets/Features/Game-Over/EndAnimation.cs
@@ -15,13 +15,13 @@ public class EndAnimation : MonoBehaviour
         for (int i = 0; i < images.Length; i++)
         { _spriteRenderers[i] = images[i].GetComponent<SpriteRenderer>(); }
         int num = Singleton.Global.Random.Next(2);
-        if (num == 0 || num == 1)
+        if (num == 0)
         { // blow up
             StartCoroutine(BlownUp(blownUpTime));
         }
         else
         { // man fired
-            //StartCoroutine(Fired(firedTime));
+            StartCoroutine(Fired(firedTime));
         }
     }
 
@@ -31,5 +31,11 @@ public class EndAnimation : MonoBehaviour
         yield return new WaitForSecondsRealtime(time);
         images[2].SetActive(true);
         images[1].SetActive(false);
+    }
+    private IEnumerator Fired(float time)
+    {
+        images[0].SetActive(true);
+        yield return new WaitForSecondsRealtime(time);
+        images[3].SetActive(true);
     }
 }
