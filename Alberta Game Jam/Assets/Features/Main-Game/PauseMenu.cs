03cc387571d8bd85145199ff944ac31e808c2c38
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-08-10 20:31:34 -0600

    testing pause menu

--- Alberta Game Jam/Assets/Features/Main-Game/PauseMenu.cs
+++ Alberta Game Jam/Assets/Features/Main-Game/PauseMenu.cs
@@ -6,6 +6,20 @@ public class PauseMenu : MenuController
 {
     public GameObject settingsCanvas;
     public GameObject mainPause;
+    public GameObject GameBoard;
+
+    internal override void OnEnable()
+    {
+        base.OnEnable();
+        GameBoard.SetActive(false);
+    }
+
+    internal override void OnDisable()
+    {
+        base.OnDisable();
+        GameBoard.SetActive(true);
+    }
+
     internal override void Awake()
     {
         base.Awake();
