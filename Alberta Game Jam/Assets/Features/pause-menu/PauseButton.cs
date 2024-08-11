3f50a51bd7ba8eaa9383fdea11fb2284cdf53c6c
RandyButternubszz <jsabandal2@nait.ca>
2024-08-10 19:05:41 -0600

    Implemented Pause Menus

---
+++ Alberta Game Jam/Assets/Features/pause-menu/PauseButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    public static Singleton Global { get; private set; }   
+    public GameManager GameManager { get; private set; }
+
+    
+}
---
+++ Alberta Game Jam/Assets/Features/pause-menu/PauseMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MenuController
+{
+    public GameObject settingsCanvas;
+    internal override void Awake()
+    {
+        base.Awake();
+
+        _buttons[0].onClick.AddListener(() => Outro(2, null, new Vector2(0, -12))); // resume button
+        _buttons[1].onClick.AddListener(() => Outro(2, settingsCanvas, new Vector2(0, -12))); // settings button
+        _buttons[2].onClick.AddListener(() => GameManager.ChangeScene(1));
+    }
+}
