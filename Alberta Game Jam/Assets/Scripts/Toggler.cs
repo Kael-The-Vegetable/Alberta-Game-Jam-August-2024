8104686343b90a1c84b0bc7d2d88c0f84a7cb258
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-08-10 23:49:20 -0600

    Got switches

--- Alberta Game Jam/Assets/Scripts/Toggler.cs
+++ Alberta Game Jam/Assets/Scripts/Toggler.cs
@@ -1,22 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[ExecuteInEditMode]
 public class Toggler : MonoBehaviour
 {
-    public Toggle toggle;
     public Image background;
+    public Sprite on;
+    public Sprite off;
 
-    private void Update()
-    {
-        toggle = toggle != null ? toggle : GetComponent<Toggle>();
-        background = background != null ? background : GetComponentInChildren<Image>();
-
-        if (toggle != null)
-        {
-            background.color = toggle.isOn
-                ? new Color(background.color.r, background.color.g, background.color.b, 0)
-                : new Color(background.color.r, background.color.g, background.color.b, 1);
-        }
-    }
+    public void Switch(bool val) 
+        => background.sprite = val ? on : off;
 }
