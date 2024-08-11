f7dd669a299f42065c62486d2d74b724eaca9685
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-08-11 11:10:36 -0600

    Update ClonkSound.cs

--- Alberta Game Jam/Assets/Scripts/ClonkSound.cs
+++ Alberta Game Jam/Assets/Scripts/ClonkSound.cs
@@ -6,10 +6,19 @@ using UnityEngine.UI;
 public class ClonkSound : MonoBehaviour
 {
     private void Awake()
-    {           
-        GetComponent<Toggle>().onValueChanged
-            .AddListener( (Boolean) =>
+    {
+        if (TryGetComponent(out Toggle toggler))
+        {
+            toggler.onValueChanged
+            .AddListener((Boolean) =>
                 Singleton.Global.FModManager.PlayOneShot(
                     Singleton.Global.FModEvents.buttonPress, transform.position));
+        }
+        else if (TryGetComponent(out Button buttoner))
+        {
+            buttoner.onClick.AddListener(() =>
+                Singleton.Global.FModManager.PlayOneShot(
+                    Singleton.Global.FModEvents.buttonPress, transform.position));
+        }
     }
 }
