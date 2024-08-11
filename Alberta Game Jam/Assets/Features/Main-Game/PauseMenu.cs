defc8c2fc3d95fc9d5510d15209b0d1307ab0ed4
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-08-10 19:52:03 -0600

    Pause Menu

--- Alberta Game Jam/Assets/Features/Main-Game/PauseMenu.cs
+++ Alberta Game Jam/Assets/Features/Main-Game/PauseMenu.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PauseMenu : MenuController
 {
     public GameObject settingsCanvas;
+    public GameObject mainPause;
     internal override void Awake()
     {
         base.Awake();
 
-        _buttons[0].onClick.AddListener(() => Outro(2, null, new Vector2(0, -12))); // resume button
+        _buttons[0].onClick.AddListener(() => Outro(2, mainPause, new Vector2(0, -12))); // resume button
         _buttons[1].onClick.AddListener(() => Outro(2, settingsCanvas, new Vector2(0, -12))); // settings button
         _buttons[2].onClick.AddListener(() => GameManager.ChangeScene(1));
     }
