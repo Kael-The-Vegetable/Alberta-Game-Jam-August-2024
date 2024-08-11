112ab3d69621847d51f03d3854355f298c89ec11
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-08-11 13:01:10 -0600

    a

--- Alberta Game Jam/Assets/Scripts/Events/NumberConversion.cs
+++ Alberta Game Jam/Assets/Scripts/Events/NumberConversion.cs
@@ -1,9 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using static ThreeColours;
 
 public class NumberConversion : GameEventTrigger
 {
